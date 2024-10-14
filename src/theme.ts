7ba7cd46593ab39cb9ea2d717e3b93b7379403f7
João <joao@roboflow.com>
2024-10-14 14:04:50 -0300

    purboflow as primary color

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,10 @@
+import { generateColors } from '@mantine/colors-generator';
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    purboflow: generateColors('#6706CE'),
+    aquavision: generateColors('#00FFCE'),
+  },
+  primaryColor: 'purboflow',
 });
