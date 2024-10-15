865233b705ea6f2b7a8653062eec4ea404510cb5
João <joao@roboflow.com>
2024-10-15 10:08:30 -0300

    move stuff around + font mono

--- src/components/Renderer/helpers/index.ts
+++ src/components/Renderer/helpers/index.ts
@@ -2,7 +2,7 @@ import { lazy } from 'react';
 import PropertyParser from './components/properties/parser';
 
 const interfaceMap: Record<string, React.LazyExoticComponent<React.ComponentType<any>>> = {
-  WorkflowInterface: lazy(() => import('../../../interfaces/WorkflowInterface')),
+  WorkflowInterface: lazy(() => import('../../../interfaces/WorkflowInterface/WorkflowInterface')),
 };
 
 export { interfaceMap, PropertyParser };
