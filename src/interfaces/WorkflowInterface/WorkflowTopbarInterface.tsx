e38c539bad48294d62ed0344e4d48c4d1be5e194
João <joao@roboflow.com>
2024-10-15 16:29:36 -0300

    init topbar

--- src/interfaces/WorkflowInterface/WorkflowTopbarInterface.tsx
+++ src/interfaces/WorkflowInterface/WorkflowTopbarInterface.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Group, Image, Text } from '@mantine/core';
 import { Workflow } from '@/types/workflow';
 
 export interface WorkflowTopbarInterfaceProps {
@@ -6,7 +7,14 @@ export interface WorkflowTopbarInterfaceProps {
 }
 
 const WorkflowTopbarInterface: React.FC<WorkflowTopbarInterfaceProps> = ({ workflow }) => {
-  return <div>WorkflowTopbarInterface</div>;
+  return (
+    <Box p="xs" className="border-b border-gray-200">
+      <Group gap="xs">
+        <Image src="https://app.roboflow.com/images/logomark-color-white-fill.svg" width={24} />
+        <Text className="text-gray-900 font-medium text-sm">{workflow.name}</Text>
+      </Group>
+    </Box>
+  );
 };
 
 export default WorkflowTopbarInterface;
